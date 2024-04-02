889cc54952a228d30a46a79c0950ab52f836040f
hamidrgholipour <105815785+hamidrgholipour@users.noreply.github.com>
2024-04-02 18:03:14 +0330

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -4,5 +4,5 @@
 vi /etc/exports
 systemctl restart nfs-server
 showmount -e nfs
-echo "192.168.10.200:/nfs/nexus /nfs/nexus/ nfs defaults 0 0" >> /etc/fstab
+echo "192.168.1.1:/nfs/nexus /nfs/nexus/ nfs defaults 0 0" >> /etc/fstab
 sudo mount -a
